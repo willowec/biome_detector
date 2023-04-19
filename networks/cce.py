66b23552d5bcbe5a924f4a20b6e3aeea7db99939
yoctometric <willcunningham64@gmail.com>
2023-04-19 19:37:58 -0400

    Added complement cross entropy loss module

---
+++ networks/cce.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CCE(nn.Module):
+    """
+    Complement Cross Entropy loss, from https://github.com/unique-chan/Complement-Cross-Entropy
+    The results of a paper by Yechan Kim, Younkwan Lee, and Moongu jeon:
+        https://www.sciencedirect.com/science/article/pii/S016786552100266X?via%3Dihub
+    """
+
+    def __init__(self, device, balancing_factor=1):
+        super(CCE, self).__init__()
+        self.nll_loss = nn.NLLLoss()
+        self.device = device # {'cpu', 'cuda:0', 'cuda:1', ...}
+        self.balancing_factor = balancing_factor
+
+    def forward(self, yHat, y):
+        # Note: yHat.shape[1] <=> number of classes
+        batch_size = len(y)
+        # cross entropy
+        cross_entropy = self.nll_loss(F.log_softmax(yHat, dim=1), y)
+        # complement entropy
+        yHat = F.softmax(yHat, dim=1)
+        Yg = yHat.gather(dim=1, index=torch.unsqueeze(y, 1))
+        Px = yHat / (1 - Yg) + 1e-7
+        Px_log = torch.log(Px + 1e-10)
+        y_zerohot = torch.ones(batch_size, yHat.shape[1]).scatter_(
+            1, y.view(batch_size, 1).data.cpu(), 0)
+        output = Px * Px_log * y_zerohot.to(device=self.device)
+        complement_entropy = torch.sum(output) / (float(batch_size) * float(yHat.shape[1]))
+
+        return cross_entropy - self.balancing_factor * complement_entropy
